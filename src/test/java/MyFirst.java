9f5486732250fe498565ba49ce09f82c7ff0f4f1
mohammedyassinAa <Yssnmed@gmail.com>
2026-03-24 17:37:48 +0100

    Nespresso Testing

--- src/test/java/MyFirst.java
+++ src/test/java/MyFirst.java
@@ -1,3 +1,5 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
@@ -15,17 +17,27 @@ public class MyFirst {
                 "C:\\chromedriver.exe");
 
         driver = new ChromeDriver();  // ✅ Now initialize AFTER setting property
-        driver.get("https://selenium.dev");
+        driver.get("https://www.nespresso.com/fr");
         driver.manage().window().maximize();
     }
 
     @Test
-    public void Test() {
-        var title = driver.getTitle();
-        System.out.println(Optional.ofNullable(title));
-        Assert.assertEquals(title, "Selenium");
+    public void test() {
+        acceptCookies();
+        clickDropdown();
     }
 
+    public void acceptCookies() {
+        WebElement cookiesButton = driver.findElement(By.id("onetrust-accept-btn-handler"));
+//        WebElement btn = driver.findElement(By.className("onetrust-accept-btn-handler"));
+//        btn.click();
+        cookiesButton.click();
+    }
+
+    public void clickDropdown() {
+        WebElement element = driver.findElement(By.cssSelector(".AccessibleLink.HeaderNavigationBarDropdown__medium-link"));
+        element.click();
+    }
     @AfterTest
     public void closeSession() {
         driver.quit();
